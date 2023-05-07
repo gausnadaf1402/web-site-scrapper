da8f84726ad50390c5f35aff49ef08d05e1e8aac
md gaus <nadafgaus1402@gmail.com>
2023-05-07 23:01:11 +0530

    Initial commit

---
+++ myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Link
+
+# Register your models here.
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display=['name','address']
